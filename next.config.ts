0618014a4d60db4258332d5c4425c97a009dec1d
Rahat Sayyed <70068895+rahatsayyed@users.noreply.github.com>
2025-04-07 10:29:22 +0530

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // output: "export",
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+      topLevelAwait: true,
+      layers: true,
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
